ec282c8742f9112dd5c0b29bc29d0aaef6c80a65
Vitor Jordão <srvitorlol@gmail.com>
2019-10-12 19:33:28 -0300

    Adicionando Partes fe funçoes e eventos

--- JS/Controller.js
+++ JS/Controller.js
@@ -1,4 +1,46 @@
 let cadastrar = () => {
     event.preventDefault();
-    console.log("teste");
+    let pessoa = criaPessoa();
+    console.log(pessoa);
 };
+
+let criaPessoa = () => {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let tipo1 = document.getElementById("tipo1").checked;
+    let tipo2 = document.getElementById("tipo2").checked;
+    let male = document.getElementById("male").checked;
+    let female = document.getElementById("female").checked;
+    let isMale;
+    if(male){
+        isMale = true;
+    } else if (female) {
+        isMale = false;
+    }
+
+    return new Pessoa(name, email, age, tipo1, tipo2, isMale);
+}
+
+let valida = (event) => {
+    let elm = event.target;
+    if(elm.id === "email"){
+        let alerta = "";
+        if(!elm.value.includes("@")){
+            alerta += '* É necessário ter "@" no email<br>';
+        }
+        if(!elm.value.includes(".")){
+            alerta += '* É necessário ter "." no email<br>';
+        }
+        
+        let div = document.getElementById("alerta");
+        if(alerta){
+            div.innerHTML = alerta;
+        } else {
+            div.innerHTML = "";
+        }
+    }
+}
+
+var form = document.getElementsByTagName("form")[0];
+form.addEventListener("keydown", valida); 
